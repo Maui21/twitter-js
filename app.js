65d55764d1c3c8356dcc4ca1791b79c118275e15
Gabriel <gabriel.wrowe@gmail.com>
2017-03-07 00:53:01 -0500

    fix

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const nJucks = require('nunjucks');
+
 app.set('port', 3000); 
 
-
-app.use(function (req, res, next) {
-    // do your logging here
-    console.log('Request Type:', req.method);
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-    next();
-});
-
-
-app.get('/', function(req, res, next) {
-
-	res.send('Welcome!');
-});
+//copy and pasted from setup
+// app.use(function (req, res, next) {
+//     // do your logging here
+//     console.log('Request Type:', req.method);
+//     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+//     next();
+// });
 
 
-app.listen(app.get('port'), function () {
-	console.log('Express started on http://localhost:' + app.get('port'));
-});
+// app.get('/', function(req, res, next) {
+
+// 	res.send('Welcome!');
+// });
+
+
+// app.listen(app.get('port'), function () {
+// 	console.log('Express started on http://localhost:' + app.get('port'));
+// });
