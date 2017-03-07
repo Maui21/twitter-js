52fbbe083ee1b72b846c8f5566d0069cf07cd4a8
Judah Hamaoui <ayhamaoui@gmail.com>
2017-03-07 12:18:16 -0500

    twitter rendered

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
 const app = express();
-
 const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank');
+const routes = require('./routes');
+
+
+app.use('/', routes);
+
 
 // var locals = {
 // 	title : 'Magical People', 
@@ -13,7 +17,7 @@ const nunjucks = require('nunjucks');
 // 	]
 // };
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -26,32 +30,6 @@ nunjucks.configure('views', {noCache: true});
 
 // nunjucks.configur('views', )
 
-app.get('/is-anybody-in-there', function(req, res, next) {
-	console.log(req.method, '/is-anybody-in-there');
-	next();
-})
-
-app.post('/modernism', function(req, res, next){
-	console.log(req.method, '/modernism');
-	next()
-})
-
-app.get('/', function(req, res, next) {
-
-
-	res.statusCode = 200;
-
-	console.log(res.statusCode);
-
-	next();
-	// console.log(res.status());
-	// res.send('Welcome!');
-});
-
-app.use(function(req,res,next){
-    console.log('Request type:', req.method)
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
 
 
 // app.set('port', 3000); 
